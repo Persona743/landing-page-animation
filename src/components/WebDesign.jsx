d052ab081717840a7c50266980c92b712da48463
Persona743 <siyavashshahidi7@gmail.com>
2023-05-05 20:50:23 +0430

    3d gltf added

---
+++ src/components/WebDesign.jsx
@@ -0,0 +1,15 @@
+import { OrbitControls, Stage } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+import Controller from './Controller';
+
+export default function WebDesign() {
+    return (
+        <Canvas>
+            <Stage environment={'city'} intensity={0.6}>
+                <Controller />
+            </Stage>
+            <OrbitControls enableZoom={false} />
+        </Canvas>
+    );
+}
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import WebDesign from './WebDesign';
+import Development from './Development';
+import ProductDesign from './ProductDesign';
 
 const data = [
     'Web Design',
@@ -72,19 +76,32 @@ const Right = styled.div`
 `;
 
 export default function Works() {
+    const [work, setWork] = useState('Web Design');
     return (
         <Section id="works">
             <Container>
                 <Left>
                     <List>
                         {data.map((item) => (
-                            <ListItem key={item} text={item}>
+                            <ListItem
+                                key={item}
+                                text={item}
+                                onClick={() => setWork(item)}
+                            >
                                 {item}
                             </ListItem>
                         ))}
                     </List>
                 </Left>
-                <Right></Right>
+                <Right>
+                    {work === 'Web Design' ? (
+                        <WebDesign />
+                    ) : work === 'Development' ? (
+                        <Development />
+                    ) : (
+                        <ProductDesign />
+                    )}
+                </Right>
             </Container>
         </Section>
     );
